46ee3f12ffa8307d8385f341e691cec302f8cb02
Luiz Sanches <luizgrsanches@gmail.com>
2016-04-12 20:14:34 -0300

    add details of issues

--- app/controllers/issue_reports_controller.rb
+++ app/controllers/issue_reports_controller.rb
@@ -15,6 +15,26 @@ class IssueReportsController < ApplicationController
       params[:issue_reports_due_date]
     )
 
+    @resolved_issues = issues_list('due_date', true)
+
+    @resolved_details = ProdigaIssueReport.details(@resolved_issues)
+
+    @unresolved_issues = issues_list('start_date', false)
+
+    @unresolved_details = ProdigaIssueReport.details(@unresolved_issues)
+
     render :index
+  end
+
+  private
+
+  def issues_list(field, is_closed)
+    ProdigaIssueReport.list(
+      field,
+      is_closed,
+      @project.id,
+      params[:issue_reports_start_date],
+      params[:issue_reports_due_date]
+    )
   end
 end
--- app/models/prodiga_issue_report.rb
+++ app/models/prodiga_issue_report.rb
@@ -1,3 +1,9 @@
+require 'holidays/core_extensions/date'
+
+class Date
+  include Holidays::CoreExtensions::Date
+end
+
 class ProdigaIssueReport < ActiveRecord::Base
   unloadable
 
@@ -16,13 +22,11 @@ class ProdigaIssueReport < ActiveRecord::Base
       results = get_summary(filter, is_closed, project_id, start_date, due_date)
 
       results.each do |result|
-        if !result['severity'].blank?
-          severity = result['severity'].to_i
+        severity = result['severity'].to_i
 
-          status = is_closed ? 'resolved' : 'unresolved'
+        status = is_closed ? 'resolved' : 'unresolved'
 
-          occurrences[severity][status] = occurrences[severity][status] + result['count'].to_i
-        end
+        occurrences[severity][status] = occurrences[severity][status] + result['count'].to_i
       end
     end
 
@@ -32,15 +36,80 @@ class ProdigaIssueReport < ActiveRecord::Base
   def self.get_summary(filter, is_closed, project_id, start_date, due_date)
     query = ActiveRecord::Base.sanitize_sql_array(
       ["SELECT cv.value AS severity, ist.name AS status, count(*) AS count
-      FROM issues AS i
-      INNER JOIN issue_statuses AS ist ON (ist.id = i.status_id)
-      INNER JOIN custom_values AS cv ON (cv.customized_id = i.id)
-      INNER JOIN custom_fields AS cf ON (cf.id = cv.custom_field_id)
-      WHERE i.project_id = ? AND cf.name = 'Grau de severidade' AND
-      i.#{filter} BETWEEN ? AND ? AND ist.is_closed = ?
-      GROUP BY cv.value, ist.name
-      ORDER BY cv.value", project_id, start_date, due_date, is_closed])
+        FROM issues AS i
+        INNER JOIN issue_statuses AS ist ON (ist.id = i.status_id)
+        INNER JOIN custom_values AS cv ON (cv.customized_id = i.id)
+        INNER JOIN custom_fields AS cf ON (cf.id = cv.custom_field_id)
+        WHERE i.project_id = ? AND cf.name = 'Grau de severidade' AND
+        i.#{filter} BETWEEN ? AND ? AND ist.is_closed = ? AND cv.value != ''
+        GROUP BY cv.value, ist.name
+        ORDER BY cv.value", project_id, start_date, due_date, is_closed])
 
     ActiveRecord::Base.connection.select_all(query)
+  end
+
+  def self.list(filter, is_closed, project_id, start_date, due_date)
+    query = ActiveRecord::Base.sanitize_sql_array(
+      ["SELECT i.*, cv.value AS severity, ist.name AS status
+        FROM issues AS i
+        INNER JOIN issue_statuses AS ist ON (ist.id = i.status_id)
+        INNER JOIN custom_values AS cv ON (cv.customized_id = i.id)
+        INNER JOIN custom_fields AS cf ON (cf.id = cv.custom_field_id)
+        WHERE i.project_id = ? AND cf.name = 'Grau de severidade' AND
+        i.#{filter} BETWEEN ? AND ? AND ist.is_closed = ? AND cv.value != ''
+        ORDER BY cv.value, i.#{filter}",
+        project_id, start_date, due_date, is_closed])
+    Issue.find_by_sql(query)
+  end
+
+  def self.details(issues)
+    issue_status = {}
+    IssueStatus.all.each { |issue| issue_status[issue.id.to_s] = issue.name }
+
+    histories, hours = {}, {}
+
+    issues.each do |issue|
+      journals = issue.journals.joins(:details).includes(:details)
+                      .where("journal_details.prop_key = 'status_id'")
+                      .order(:created_on)
+      if !journals.blank?
+        details = ""
+        journals.each do |journal|
+          journal.details.each do |detail|
+            details += "#{format_time(journal.created_on)} - #{issue_status[detail.value]}<br/>"
+          end
+          histories[issue.id] = details
+        end
+
+        time = 0
+        (journals.first.created_on.to_date..journals.last.created_on.to_date).each do |date|
+          if !date.saturday? && !date.sunday? && !date.holiday?(:br)
+            time += 8
+          end
+        end
+
+        first_hour = journals.first.created_on.to_time.to_s[11..12].to_i
+
+        first_status = issue_status[journals.first.details.first.value]
+
+        if first_hour < 18
+          if first_status.downcase == 'fechada'
+            time = first_hour - 8
+          else
+            time -= 18 - first_hour
+          end
+        end
+
+        if journals.first.created_on.to_date != journals.last.created_on.to_date
+          last_hour = journals.last.created_on.to_time.to_s[11..12].to_i
+
+          time -= 18 - last_hour if last_hour < 18
+        end
+
+        hours[issue.id] = time
+      end
+    end
+
+    {hours: hours, histories: histories}
   end
 end
