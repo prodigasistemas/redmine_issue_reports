3c4658c28fb87172c64af1661acd8222a339e40d
Luiz Sanches <luizgrsanches@gmail.com>
2016-04-15 12:17:28 -0300

    calculate hours and minutes

--- app/models/prodiga_issue_report.rb
+++ app/models/prodiga_issue_report.rb
@@ -72,36 +72,33 @@ class ProdigaIssueReport < ActiveRecord::Base
                       .where("journal_details.prop_key = 'status_id'")
                       .order(:created_on)
       if !journals.blank?
+        details = ''
+        suspended_starting_date = nil
+        discount = 0.0
+
         time = hours_period(issue.created_on, journals.last.created_on)
 
-        first_hour = issue.created_on.to_time.hour
+        first_time = "#{issue.created_on.to_time.hour}.#{issue.created_on.to_time.min}".to_f
 
-        last_hour = journals.last.created_on.to_time.hour
+        last_time = "#{journals.last.created_on.to_time.hour}.#{journals.last.created_on.to_time.min}".to_f
 
         first_status = issue_status[journals.first.details.first.value]
 
         if issue.created_on.to_date == journals.last.created_on.to_date
-          calc = last_hour - first_hour
-          calc -= 2 if first_hour < 12
-          time = calc > 0 ? calc : 1
+          time = subtract_time(last_time, first_time)
+          discount += 2.0 if first_time.to_i < 12
         else
-          if first_hour < 18
+          if first_time.to_i < 18
             if first_status.downcase == 'fechada'
-              time = first_hour - 8
+              time = subtract_time(first_time, 8.0)
             else
-              time -= 18 - first_hour
+              discount = sum_time(discount, subtract_time(18.0, first_time))
             end
           end
-        end
 
-        if issue.created_on.to_date != journals.last.created_on.to_date
-          time -= 18 - last_hour if last_hour < 18
-          time -= 2 if last_hour < 12
+          discount = sum_time(discount, subtract_time(18.0, last_time)) if last_time.to_i < 18
+          discount += 2.0 if last_time.to_i < 12
         end
-
-        details = ''
-        suspended_starting_date = nil
-        discount = 0
 
         journals.each do |journal|
           journal.details.each do |detail|
@@ -115,7 +112,24 @@ class ProdigaIssueReport < ActiveRecord::Base
             if old_value.downcase == 'suspensa' && !suspended_starting_date.nil?
               suspended_end_date = journal.created_on
 
-              discount = hours_period(suspended_starting_date, suspended_end_date)
+              suspended_time = hours_period(suspended_starting_date, suspended_end_date)
+
+              suspended_discount = 0.0
+
+              suspended_first_time = "#{suspended_starting_date.to_time.hour}.#{suspended_starting_date.to_time.min}".to_f
+
+              suspended_last_time = "#{suspended_end_date.to_time.hour}.#{suspended_end_date.to_time.min}".to_f
+
+              if suspended_starting_date.to_date == suspended_end_date.to_date
+                suspended_time = subtract_time(suspended_last_time, suspended_first_time)
+                suspended_discount += 2.0 if suspended_first_time.to_i < 12
+              else
+                suspended_discount = sum_time(suspended_discount, subtract_time(18.0, suspended_first_time)) if suspended_first_time.to_i < 18
+                suspended_discount = sum_time(suspended_discount, subtract_time(18.0, suspended_last_time)) if suspended_last_time.to_i < 18
+                suspended_discount += 2.0 if suspended_last_time.to_i < 12
+              end
+
+              discount = sum_time(discount, subtract_time(suspended_time, suspended_discount))
 
               suspended_starting_date = nil
             end
@@ -124,7 +138,7 @@ class ProdigaIssueReport < ActiveRecord::Base
           histories[issue.id] = details
         end
 
-        hours[issue.id] = time - discount
+        hours[issue.id] = subtract_time(time, discount)
       end
     end
 
@@ -132,12 +146,56 @@ class ProdigaIssueReport < ActiveRecord::Base
   end
 
   def self.hours_period(start_date, due_date)
-    hours = 0
+    hours = 0.0
     (start_date.to_date..due_date.to_date).each do |date|
-      if !date.saturday? && !date.sunday? && !date.holiday?(:br)
-        hours += 8
-      end
+      hours += 8.0 if !date.saturday? && !date.sunday? && !date.holiday?(:br)
     end
     hours
+  end
+
+  def self.subtract_time(_minuend, _subtrahend)
+    minuend = float_to_array(_minuend)
+    subtrahend = float_to_array(_subtrahend)
+
+    minuend[1] = minuend[1] * 10 if minuend[1] <= 5
+    subtrahend[1] = subtrahend[1] * 10 if subtrahend[1] <= 5
+
+    if minuend[1] < subtrahend[1]
+      minuend[0] -= 1
+      minuend[1] += 60
+    end
+
+    minutes = minuend[1] - subtrahend[1]
+
+    hours = minuend[0] - subtrahend[0]
+
+    "#{hours}.#{minutes}".to_f
+  end
+
+  def self.sum_time(value1, value2)
+    value_a = float_to_array(value1)
+    value_b = float_to_array(value2)
+
+    value_a[1] = value_a[1] * 10 if value_a[1] <= 5
+    value_b[1] = value_b[1] * 10 if value_b[1] <= 5
+
+    minutes = value_a[1] + value_b[1]
+
+    if minutes > 60
+      value_a[0] += 1
+      minutes = minutes - 60
+    end
+
+    hours = value_a[0] + value_b[0]
+
+    "#{hours}.#{minutes}".to_f
+  end
+
+  def self.float_to_array(value)
+    result = value.to_s.split('.')
+
+    0.upto(1) {|i| result[i] = result[i].to_i}
+
+    result
   end
 end
