0285b22108bf88c098b50c92c21e177785ad5595
Luiz Sanches <luizgrsanches@gmail.com>
2016-10-18 12:22:41 -0300

    Update update-redmine.sh

--- update-redmine/update-redmine.sh
+++ update-redmine/update-redmine.sh
@@ -3,5 +3,3 @@
 cp custom_fields.js ../../../public/javascripts
 
 echo "<%= javascript_include_tag 'custom_fields' %>" >> ../../../app/views/issues/_form.html.erb
-
-mysql -u redmine -predmine redmine < redmine_config.sql
